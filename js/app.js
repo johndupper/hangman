7069f1e9f6d75b160e9d2ee8cf601f616080e614
John Dupper <john.dupper@me.com>
2016-10-26 12:28:50 -0400

    showing game returns false if answer field left blank, otherwise generates text fields to match length of solution string

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
 // fix: answer checks for spaces only
+// fix: enter button for solution input
+
 var console, answer;
 
 function showGame() {
     'use strict';
     console.log('call showGame');
+    var i, letter;
+    for (i  = 1; i <= answer.length; i += 1) {
+        console.log(i);
+        letter = document.createElement('input');
+        letter.type = 'text';
+        letter.className = 'letter';
+        letter.id = i;
+        document.body.appendChild(letter);
+    }
+    
 }
 
 function isSolutionValid() {
     'use strict';
 
-    if (answer.includes(' ')) {
+    if (answer === '' || answer.includes(' ')) {
         console.log('not valid solution');
+        answer = '';
         return false;
     } else {
         console.log('valid solution');
+        showGame();
         return true;
     }
 }
@@ -29,9 +43,15 @@ function prepareGame() {
     if (isSolutionValid()) {
         document.getElementById('solutionWord').style.display = 'none';
         document.getElementById("solutionButton").style.display = 'none';
-        showGame();
     }
 }
 
 // call prepareGame on button click
 document.getElementById("solutionButton").addEventListener("click", prepareGame);
+document.getElementById("solutionButton").addEventListener("keyup", function (event) {
+    'use strict';
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("solutionButton").click(prepareGame);
+    }
+});
