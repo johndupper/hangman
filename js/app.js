3962815bc6bbebd426c98ae4b371a9fba07bf87f
John Dupper <john.dupper@me.com>
2016-10-29 16:45:11 -0400

    game displays correct letters, creates a list of letters not present and input fields turn green when complete word is solved; see // for fixes

--- js/app.js
+++ js/app.js
@@ -2,18 +2,43 @@
 // fix: input doesn't clear back to placeholder
 // fix: build the picture in css
 
-var alert, console, answer, guess;
+var alert, console, answer, guess, guessList, complete;
+
+
+function declareWinner() {
+    'use strict';
+    var letters, i;
+    letters = document.getElementsByClassName('letter');
+    for (i = 0; i < letters.length; i += 1) {
+        letters[i].style.backgroundColor = 'green';
+    }
+}
 
 function checkLetter() {
     'use strict';
     
-    guess = document.getElementById('userGuess').value;
+    guess = document.getElementById('userGuess').value.toLowerCase();
+    document.getElementById('userGuess').value = '';
     
-    var i;
+    var i, letterPresent;
+    letterPresent = false;
+    
     for (i = 0; i < answer.length; i += 1) {
         if (guess === answer.charAt(i)) {
+            letterPresent = true;
             document.getElementById(i).value = guess;
+            complete += 1;
+            console.log(complete);
         }
+    }
+    
+    if (!letterPresent) {
+        guessList.style.display = 'block';
+        guessList.innerHTML += guess + ' ';
+    }
+    
+    if (complete === answer.length) {
+        declareWinner();
     }
 }
 
@@ -23,6 +48,7 @@ function checkLetter() {
 function createGameHTML() {
     'use strict';
     
+    complete = 0;
     var i, attr, letter, userGuess, checkButton;
     
     for (i  = 0; i < answer.length; i += 1) {
@@ -55,6 +81,12 @@ function createGameHTML() {
     checkButton.value = 'Survey Says...';
     checkButton.addEventListener('click', checkLetter);
     document.body.appendChild(checkButton);
+    
+    guessList = document.createElement('h2');
+    guessList.id = 'guessList';
+    guessList.innerHTML = 'Letters: ';
+    guessList.style.display = 'none';
+    document.body.appendChild(guessList);
 }
 
 // hide solution input
